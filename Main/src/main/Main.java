1ede439afe66d459b2152bc1f2d185bec89cd0a5
chicho377 <morafabricio86@gmail.com>
2024-10-01 16:36:01 -0600

    Agregando scanner, variable opcion y el ciclo do while para el menu

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -1,6 +1,8 @@
 
 package main;
 
+import java.util.Scanner;
+
 /*
  * Fabricio Mora Gomez
  * Examen 1
@@ -14,6 +16,16 @@ public class Main {
     
     // funcion para el menu
     public static void menu(){
-    
+        
+        // objeto scanner para obtener los datos desde la consola
+        Scanner scanner = new Scanner(System.in);
+        
+        // variable opcion para el ciclo del menu
+        int opcion;
+        
+        // ciclo do while para el menu
+        do {
+            
+        } while (opcion != 3);
     }
 }
